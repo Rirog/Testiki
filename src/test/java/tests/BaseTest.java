55bb2e84a2652ba80b2bee557f215b3aa3670d5a
Rirog <vitalijmaskov5135@gmail.com>
2026-02-14 16:21:22 +0300

    TEst

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,6 @@ public class BaseTest {
 
     protected static String projectId = "3297";
     protected static String projectSlug = "duplingha-neon-cloud";
-    protected static String collectionSlug = "testCollections";
+    protected static String collectionSlug = "testcollections";
     protected static String email = "duplingha@gmail.com";
 }
