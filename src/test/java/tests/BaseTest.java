988080d1e8593de0a7f1c20811c2c3f46e414014
Rirog <vitalijmaskov5135@gmail.com>
2026-02-14 16:08:55 +0300

    TEst

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,8 +2,8 @@ package tests;
 
 public abstract class BaseTest {
 
-    protected final String projectId = "3297";
-    protected final String projectSlug = "duplingha-neon-cloud";
-    protected final String collectionSlug = "testCollections";
-    protected final String email = "Testiki@gmail.com";
+    protected static String projectId = "3297";
+    protected static String projectSlug = "duplingha-neon-cloud";
+    protected static String collectionSlug = "testCollections";
+    protected static String email = "Testiki@gmail.com";
 }
