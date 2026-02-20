b1a6f7749daa2b0494854239a36cf1692367ea85
Rirog <vitalijmaskov5135@gmail.com>
2026-02-20 14:21:35 +0300

    NE NADO..............

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,12 +5,15 @@ import lombok.Setter;
 public abstract class BaseTest {
 
     protected String projectId = "3503";
-//    protected String projectSlug = "dupling-cocoa-studio";
+
     @Setter
     protected String collectionSlug;
+
     protected String email = "duplingha@gmail.com";
+
     @Setter
     protected String recordId;
+
     @Setter
     protected String record = "testiki";
 }
