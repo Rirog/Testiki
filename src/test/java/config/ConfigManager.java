21232a08445187a05dacfcff5c7ee0a31c88d809
Rirog <vitalijmaskov5135@gmail.com>
2026-02-28 17:27:45 +0300

    fix resource

--- src/test/java/config/ConfigManager.java
+++ src/test/java/config/ConfigManager.java
@@ -5,15 +5,6 @@ import org.aeonbits.owner.ConfigFactory;
 public class ConfigManager {
     private static final ValuesConfig factory = ConfigFactory.create(ValuesConfig.class);
 
-    static {
-        try {
-            System.out.println("Trying to load config...");
-            System.out.println("Classpath: " + System.getProperty("java.class.path"));
-            System.out.println("Resource URL: " + ConfigManager.class.getResource("/values.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     public static String getApiKey() {
         return System.getProperty(factory.token());
     }
